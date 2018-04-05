3518525c6d584dc0b454698692d9d03ff6fa8a59
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-04-05 11:44:07 -0400

    burger image added

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    //   console.log(props);
+    //   let transformedIngredients = Object.keys(props.ingredients)
+    //     .map(igkey => {
+    //       return [...Array(props.ingredients[igkey])].map((_, i) => {
+    //         return <BurgerIngredient key={igkey + i} type={igkey} />;
+    //       });
+    //     })
+    //     .reduce((arr,el) => {
+    //       return arr.concat(el)
+    //     }, []);
+    //     if (transformedIngredients.length === 0) {
+    //         transformedIngredients = <p>Please start adding ingredients!</p>;
+    //     }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="salad" />
+            {/* {transformedIngredients} */}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+
+export default burger;
